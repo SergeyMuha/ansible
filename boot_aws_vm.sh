02aba9e33cf77abea320669a22e920007ad66513
Sergey Muha <muchasergey@gmail.com>
2018-05-11 16:57:28 +0300

    Files have been updated

--- boot_aws_vm.sh
+++ boot_aws_vm.sh
@@ -1,6 +1,28 @@
 #! /bin/bash
-id=$(aws ec2 run-instances --image-id ami-38708b45 --key-name aws_console_key --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=name,Value=ansiblehost}]' | grep -i InstanceId  | awk -F':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | sed 's/ //g')
-sleep 15
+if [ -z "$1" ]; then 
+	echo usage: boot_aws_vm.sh amis
+	echo amis : ami-38708b45 - ubuntu 14.04, ami-a0d009dd - ubuntu 16.04, ami-467ca739 - amazon
+	exit
+fi
+
+id=$(aws ec2 run-instances --image-id $1 --key-name aws_console_key --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=OStype,Value=$1}]" | grep -i InstanceId  | awk -F':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | sed 's/ //g')
+sleep 10
 pubdns=$(aws ec2 describe-instances --instance-ids $id | grep -m 1 -i publicdns  | awk -F':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | sed 's/ //g')
 echo $pubdns
-echo "$pubdns ansible_ssh_private_key_file=~/.ssh/aws_console_key.pem" >> /etc/ansible/hosts
+#echo "$pubdns ansible_ssh_private_key_file=~/.ssh/aws_console_key.pem" >> /etc/ansible/hosts
+python=" "
+if [ "$1" = "ami-38708b45" ]
+then
+	ostype=apt 
+elif [ "$1" = "ami-a0d009dd" ]
+then
+	ostype=apt
+	python='ansible_python_interpreter=/usr/bin/python3'
+else                
+	ostype=yum
+fi
+
+cat <<EOF >> /etc/ansible/hosts
+[server-$ostype]
+$pubdns ansible_ssh_private_key_file=~/.ssh/aws_console_key.pem $python
+EOF
