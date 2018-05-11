680442e2905f733d16ca1592737784000ed13667
Sergey Muha <muchasergey@gmail.com>
2018-05-11 17:09:46 +0300

    key-name

--- boot_aws_vm.sh
+++ boot_aws_vm.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 if [ -z "$1" ]; then 
-	echo usage: boot_aws_vm.sh amis
+	echo usage: boot_aws_vm.sh amis aws-key-name
 	echo amis : ami-38708b45 - ubuntu 14.04, ami-a0d009dd - ubuntu 16.04, ami-467ca739 - amazon
+        echo You could check your aws-key-name with $ aws ec2 describe-key-pairs
 	exit
 fi
 
-id=$(aws ec2 run-instances --image-id $1 --key-name aws_console_key --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=OStype,Value=$1}]" | grep -i InstanceId  | awk -F':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | sed 's/ //g')
+id=$(aws ec2 run-instances --image-id $1 --key-name $2 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=OStype,Value=$1}]" | grep -i InstanceId  | awk -F':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | sed 's/ //g')
 sleep 10
 pubdns=$(aws ec2 describe-instances --instance-ids $id | grep -m 1 -i publicdns  | awk -F':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | sed 's/ //g')
 echo $pubdns
